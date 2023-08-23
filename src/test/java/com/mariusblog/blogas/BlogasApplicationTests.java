36a3dfea6efaa9c037ec46b30b9b737d57433d67
MZenkevicius <mzenkevicius@yahoo.com>
2023-08-23 20:28:47 +0300

    First commit

---
+++ src/test/java/com/mariusblog/blogas/BlogasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariusblog.blogas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
